641c6b8009c6cd8e2103884d642128a64ddf95f5
PJ Frias <peejfrias@gmail.com>
2018-01-23 13:59:20 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ class Pokemon
 
   def self.find(id, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).first
-    pokemon_hash = 
+    pokemon_hash = {}
     self.new(pokemon)
   end
 end
