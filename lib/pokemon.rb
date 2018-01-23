2c34949855299426fa8280449ec5959f9979f5b6
PJ Frias <peejfrias@gmail.com>
2018-01-23 03:48:05 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,10 +4,10 @@ class Pokemon
 
   def initialize(pokemon)
     binding.pry
-    @id = pokemon.id
-    @name = pokemon.name
-    @type = pokemon.type
-    @db = pokemon.db
+    @id = pokemon[:id]
+    @name = pokemon[:name]
+    @type = pokemon[:type]
+    @db = pokemon[:db]
   end
 
   def self.save
